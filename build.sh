d72f3bbc795c41c09e71c445aed499b36b14b4b1
ripples <ripples.alive@outlook.com>
2026-04-23 10:02:32 +0000

    Upgrade Beszel to 0.18.7 and fix build proxying

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 NAME=beszel
 BUILDER=${NAME}-builder
-VERSION=0.18.4
+VERSION=0.18.7
 
 if [ ! -d "beszel" ]; then
     git clone https://github.com/henrygd/beszel
@@ -12,21 +12,57 @@ cd beszel
 git fetch
 git reset --hard
 git checkout "v${VERSION}"
-git apply -3 ../enable-proxy.diff
+git apply ../enable-proxy.diff
 
 make build-web-ui
 
-docker buildx create --use --name $BUILDER
+docker buildx rm "$BUILDER" >/dev/null 2>&1 || true
+
+set -- docker buildx create --use --name "$BUILDER"
+
+if [ -n "${http_proxy:-${HTTP_PROXY:-}}" ]; then
+    set -- "$@" --driver-opt "env.http_proxy=${http_proxy:-$HTTP_PROXY}"
+    set -- "$@" --driver-opt "env.HTTP_PROXY=${HTTP_PROXY:-$http_proxy}"
+fi
+
+if [ -n "${https_proxy:-${HTTPS_PROXY:-}}" ]; then
+    set -- "$@" --driver-opt "env.https_proxy=${https_proxy:-$HTTPS_PROXY}"
+    set -- "$@" --driver-opt "env.HTTPS_PROXY=${HTTPS_PROXY:-$https_proxy}"
+fi
+
+if [ -n "${no_proxy:-${NO_PROXY:-}}" ]; then
+    set -- "$@" --driver-opt "env.no_proxy=${no_proxy:-$NO_PROXY}"
+    set -- "$@" --driver-opt "env.NO_PROXY=${NO_PROXY:-$no_proxy}"
+fi
+
+"$@"
 docker buildx inspect --bootstrap
 
-docker buildx build \
+set -- docker buildx build \
     --platform linux/amd64,linux/arm64 \
     --push \
     --pull \
-    --tag ripples/$NAME:$VERSION \
-    --build-arg VERSION=$VERSION \
-    --builder $BUILDER . \
+    --tag "ripples/$NAME:$VERSION" \
+    --build-arg "VERSION=$VERSION" \
+    --builder "$BUILDER" . \
     -f internal/dockerfile_hub
 
-docker buildx stop $BUILDER
-docker buildx rm $BUILDER
+if [ -n "${http_proxy:-${HTTP_PROXY:-}}" ]; then
+    set -- "$@" --build-arg "http_proxy=${http_proxy:-$HTTP_PROXY}"
+    set -- "$@" --build-arg "HTTP_PROXY=${HTTP_PROXY:-$http_proxy}"
+fi
+
+if [ -n "${https_proxy:-${HTTPS_PROXY:-}}" ]; then
+    set -- "$@" --build-arg "https_proxy=${https_proxy:-$HTTPS_PROXY}"
+    set -- "$@" --build-arg "HTTPS_PROXY=${HTTPS_PROXY:-$https_proxy}"
+fi
+
+if [ -n "${no_proxy:-${NO_PROXY:-}}" ]; then
+    set -- "$@" --build-arg "no_proxy=${no_proxy:-$NO_PROXY}"
+    set -- "$@" --build-arg "NO_PROXY=${NO_PROXY:-$no_proxy}"
+fi
+
+"$@"
+
+docker buildx stop "$BUILDER"
+docker buildx rm "$BUILDER"
